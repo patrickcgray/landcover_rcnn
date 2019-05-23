95f35783001d2dae3865e8ecb0dff940c07c7c12
Diego <diegochll@gmail.com>
2019-05-23 20:03:05 +0000

    printing labels needs to improve

--- visualize.py
+++ visualize.py
@@ -19,9 +19,9 @@ class VisualizeData:
     (11, (0,0,255)), #water ~ blue
 (12, (0,0,255)), #snow ~ white
 (21, (255,0,0)), #open space developed ~ red
-(22, (200,0,0)), # low intensity developed ~ darker red
-(23, (150,0,0)), # medium intensity developed ~ darker darker red
-(24, (100,0,0)), # high intensity developed ~ darker darker darker red
+(22, (50,0,0)), # low intensity developed ~ darker red
+(23, (50,0,0)), # medium intensity developed ~ darker darker red
+(24, (50,0,0)), # high intensity developed ~ darker darker darker red
 (31, (153,76,0)), # barren land ~ dark orange
 (41, (0,204,0)), # deciduous forest ~ green
 (42, (0,153,0)), # evergreen forest ~ darker green
@@ -56,7 +56,7 @@ class VisualizeData:
 (95, "Emergent Herbaceous Wetlands"),
 ))
    
-    def view_landsat_tile(self, landsat_index):
+    def view_landsat(self, landsat_index):
         image_dataset = self.landsat[landsat_index]
         full_img = self.landsat[landsat_index].read()
         colors_reshaped = self.__normalize_rgb(full_img)
@@ -66,11 +66,9 @@ class VisualizeData:
     
     def view_labels(self, landsat_index):
         masked_label_image, raster_poly = self.__make_a_label_mask(landsat_index)
-        fig, ax = plt.subplots(figsize=[15,15])
-        ax.imshow(masked_label_image[0,:,:])
-
+        ax = self.__plot_a_tile(masked_label_image[0,:,:], colors=self.colors)
         
-    def print_a_tile(self, landsat_index, class_index, tile_size):
+    def print_a_tile(self, landsat_index, class_index, tile_size, middle=False):
         l8_proj = Proj(self.landsat[landsat_index].crs)
         buffer = math.ceil(tile_size / 2)
         masked_label_image, raster_poly = self.__make_a_label_mask(landsat_index)
@@ -82,29 +80,62 @@ class VisualizeData:
         tile_loc = all_locations[loc_index]
         #r, c is in label mask pixel locations
         r_label, c_label = tile_loc
-        label_tile = self.__read_from_raster(self.labels, c_label+raster_poly.bounds[0]-buffer, r_label+raster_poly.bounds[1]-buffer, tile_size)
         r_landsat, c_landsat = self.__transform_to_l8(landsat_index, r_label, c_label, raster_poly)
-        tile = self.__read_from_raster(self.landsat[landsat_index], c_landsat-buffer, r_landsat-buffer, tile_size)
-        tile_normalized = self.__normalize_rgb(tile)
-        ax_c = self.__plot_a_tile(tile_normalized)
+        label_tile = self.__read_from_raster(self.labels, c_label+raster_poly.bounds[0]-buffer, r_label+raster_poly.bounds[1]-buffer, tile_size)
+        landsat_tile = self.__read_from_raster(self.landsat[landsat_index], c_landsat-buffer, r_landsat-buffer, tile_size)
+        landsat_tile_normalized = self.__normalize_rgb(landsat_tile)
+        ax_c = self.__plot_a_tile(landsat_tile_normalized)
         ax_c.set_title(self.class_names[class_index])
         ax_l = self.__plot_a_tile(label_tile[0,:,:], colors = self.colors)
         ax_l.set_title(self.class_names[class_index])
+        if middle:
+            ax_m = self.__plot_a_tile(label_tile[0,:,:], colors = self.colors, middle = True)
+            ax_m.set_title(self.class_names[class_index])
+            
+            
     
+    def print_chosen_tile(self, landsat_index, tile_size, row_col, middle=False):
+        buffer = math.ceil(tile_size / 2)
+        r_landsat, c_landsat = row_col
+        r_label, c_label = self.__transform_to_label(landsat_index, r_landsat, c_landsat)
+        class_index = self.__get_label(r_label, c_label)
+        label_tile = self.__read_from_raster(self.labels, c_label-buffer, r_label-buffer, tile_size)
+        landsat_tile = self.__read_from_raster(self.landsat[landsat_index], c_landsat-buffer, r_landsat-buffer, tile_size)
+        landsat_tile_normalized = self.__normalize_rgb(landsat_tile)
+        ax_c = self.__plot_a_tile(landsat_tile_normalized)
+        ax_c.set_title(self.class_names[class_index])
+        ax_l = self.__plot_a_tile(label_tile[0,:,:], colors = self.colors)
+        ax_l.set_title(self.class_names[class_index])
+        if middle:
+            ax_m = self.__plot_a_tile(label_tile[0,:,:], colors = self.colors, middle = True)
+            ax_m.set_title(self.class_names[class_index])
+        
+        
+    def __get_label(self, r_label, c_label):
+        window = ((r_label, r_label+1), (c_label, c_label+1))
+        data = self.labels.read(1, window=window, masked=False, boundless=True)
+        label = data[0,0]
+        return label
     def __read_from_raster(self, raster, column_start, row_start, tile_size):
         w = raster.read(window=Window(column_start, row_start, tile_size, tile_size))
         return w
-    def __plot_a_tile(self, tile, colors = None):
+    def __plot_a_tile(self, tile, colors = None, middle = False):
         fig, ax = plt.subplots(figsize=(10, 10))
         self.open_figs.append(fig)
         if colors == None:
              ax.imshow(tile)
         else:
             colored_label_img = np.zeros((tile.shape[0], tile.shape[1], 3))
+            center_px = math.ceil(tile.shape[0]/2)
             for h in range(tile.shape[0]):
                 for w in range(tile.shape[1]):
-                    colored_label_img[h][w] = np.divide(colors[tile[h][w]], 255)
-            print("sanity check print middle pixel {}".format(tile[math.ceil(h/2)][math.ceil(h/2)]))
+                    if h == center_px and w == center_px and middle:
+                        colored_label_img[h][w] = (0,0,0)
+                    if tile[h][w] not in self.class_names:
+                         colored_label_img[h][w] = (0,0,0)
+                    else:
+                        colored_label_img[h][w] = np.divide(colors[tile[h][w]], 255)
+            print("sanity check print middle pixel {}".format(tile[center_px][center_px]))
             ax.imshow(colored_label_img)
         return ax
     
@@ -121,6 +152,13 @@ class VisualizeData:
         # convert from landsat geographic space into row col
         r_landsat,c_landsat = self.landsat[landsat_index].index(x,y) 
         return r_landsat, c_landsat
+   
+    def __transform_to_label(self, landsat_index, r_landsat, c_landsat):
+        l8_proj = Proj(self.landsat[landsat_index].crs)
+        x,y = self.landsat[landsat_index].xy(r_landsat,c_landsat)
+        x,y = transform(l8_proj, self.label_proj,x,y)
+        r_label,c_label = self.labels.index(x,y) 
+        return r_label, c_label
         
     def __normalize_rgb(self, tile):
         tile = tile[[3, 2, 1],:,:].astype(np.float64)
