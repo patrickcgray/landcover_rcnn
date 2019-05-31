84de099cc3e68380e216de5413992d83ea1fe9e9
Diego <diegochll@gmail.com>
2019-05-31 04:16:37 +0000

    added FCN compatability

--- classifier_utilities.py
+++ classifier_utilities.py
@@ -626,65 +626,7 @@ def sk_tile_generator(l8_image_datasets, s1_image_datasets, dem_image_datasets, 
         return (image_batch, label_batch)
         
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.utils.multiclass import unique_labels
 
-
-def plot_confusion_matrix(y_true, y_pred, classes, class_dict,
-                          normalize=False,
-                          title=None,
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if not title:
-        if normalize:
-            title = 'Normalized confusion matrix'
-        else:
-            title = 'Confusion matrix, without normalization'
-
-    # Compute confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
-    # Only use the labels that appear in the data
-    classes = classes[unique_labels(y_true, y_pred)]
-    # convert class_id to class_name using the class_dict
-    cover_names = []
-    for cover_class in classes:
-        cover_names.append(class_dict[cover_class])
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    else:
-        pass
-    #print(cm)
-
-    fig, ax = plt.subplots(figsize=(10,10))
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-    ax.figure.colorbar(im, ax=ax)
-    # We want to show all ticks...
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
-           xticklabels=cover_names, yticklabels=cover_names,
-           title=title,
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
-    fig.tight_layout()
-    return ax
 
 """
 TO Run: 
--- tile.py
+++ tile.py
@@ -24,7 +24,7 @@ class tile_gen():
         l8_band_count = self.landsat[0].count  
         s1_band_count = self.s1[0].count
         dem_band_count = self.dem[0].count
-        total = l8_band_count + s1_band_count + dem_band_count
+        total = l8_band_count + s1_band_count + dem_band_count - 1  # take one off because we don't want the QA band
         if return_total:
             return total
         return (l8_band_count, s1_band_count, dem_band_count, total)
@@ -36,12 +36,11 @@ class tile_gen():
             tile_shape = (self.tile_length, self.tile_length, self.__get_band_counts(return_total=True))
         return tile_shape
         
-    def tile_generator(self, pixel_locations, batch_size, merge=True):
+    def tile_generator(self, pixel_locations, batch_size, fcn = False, merge=True):
     ### this is a keras compatible data generator which generates data and labels on the fly 
     ### from a set of pixel locations, a list of image datasets, and a label dataset
         tile_size = self.tile_length
         i = 0
-        bad_count = 0
         label_proj = Proj(self.label.crs)
         l8_proj = Proj(self.landsat[0].crs)
         # assuming all images have the same num of bands
@@ -49,8 +48,11 @@ class tile_gen():
         class_count = self.class_count
         buffer = math.floor(tile_size / 2)
         while True:
-            image_batch = np.zeros((batch_size, tile_size, tile_size, band_count-1)) # take one off because we don't want the QA band
-            label_batch = np.zeros((batch_size,class_count))
+            image_batch = np.zeros((batch_size, tile_size, tile_size, band_count))
+            if fcn:
+                label_batch = np.zeros((batch_size,tile_size, tile_size, class_count))
+            else:
+                label_batch = np.zeros((batch_size,class_count))
             b = 0
             while b < batch_size:
                 # if we're at the end  of the data just restart
@@ -62,11 +64,8 @@ class tile_gen():
                 tiles_to_read = [self.landsat[dataset_index], self.s1[dataset_index], self.dem[dataset_index]]
                 tile, s1_tile, dem_tile = util.read_windows(tiles_to_read, c ,r, buffer, tile_size)
                 tile = tile[0:7]
-                # reshape from raster format to image format and standardize according to image wide stats
                 reshaped_tile = (reshape_as_image(tile)  - 982.5) / 1076.5
-                # reshape from raster format to image format and standardize according to image wide stats
                 reshaped_s1_tile = (reshape_as_image(s1_tile)  - 0.10) / 0.088
-                #reshape from raster format to image format and standardize according to image wide stats
                 reshaped_dem_tile = (reshape_as_image(dem_tile)  - 31) / 16.5
                 ### get label data
                 # find gps of that pixel within the image
@@ -76,26 +75,57 @@ class tile_gen():
                     x,y = transform(l8_proj,label_proj,x,y)
                     # reference gps in label_image
                 row, col = self.label.index(x,y)
-                window = ((row, row+1), (col, col+1))
-                data = self.label.read(1, window=window, masked=False, boundless=True)
-                if merge:
+                flag = True
+                if fcn:
+                    data = self.label.read(1, window=Window(col-buffer, row-buffer, tile_size, tile_size))
                     data = util.merge_classes(data)
-                label = data[0,0]
-                if np.isnan(reshaped_s1_tile).any() or np.isinf(reshaped_s1_tile).any():
-                    bad_count+=1
-                    print(bad_count)
-                    pass
-                elif np.isnan(reshaped_dem_tile).any() or np.isinf(reshaped_dem_tile).any():
-                    bad_count+=1
-                    print(bad_count)
-                    pass             
-                elif label == 0 or np.isnan(label).any() or label not in util.class_to_index:
-                    bad_count+=1
-                    print(bad_count)
-                    pass
-                else:                   
-                    label = util.class_to_index[label]
-                    label_batch[b][label] = 1
+                    if 0 not in data and np.nan not in data:
+                        label = self.one_hot_encode(data, tile_size, class_count)
+                        label_batch[b] = label
+                        flag = False
+                else:
+                    data = self.label.read(1, window=((row, row+1), (col, col+1)), masked=False, boundless=True)
+                    data = util.merge_classes(data)
+                    if 0 not in data and np.nan not in data:
+                        label = data[0,0]                       
+                        label = util.class_to_index[label]
+                        label_batch[b][label] = 1
+                        flag = False
+                if not flag:
                     image_batch[b] = np.dstack((reshaped_tile, reshaped_s1_tile, reshaped_dem_tile))    
                     b += 1
-            yield (image_batch, label_batch)     
+            yield (image_batch, label_batch)   
+            
+    def one_hot_encode(self, data, tile_size, class_count):
+        label = np.zeros((tile_size, tile_size, class_count))
+        for i in range(tile_size):
+            for j in range(tile_size):
+                label_index = util.class_to_index[data[i][j]]
+                label[i][j][label_index] = 1
+        return label
+
+    def evaluate_cnn(self, model, pixels, batch_size=64, merge=True):
+        predictions = model.predict_generator(generator = self.tile_generator(pixels, batch_size=batch_size, merge=merge), steps=len(pixels) // batch_size, verbose=1)
+        eval_generator = self.tile_generator(pixels, batch_size=1)
+        labels = np.empty(predictions.shape)
+        count = 0
+        while count < len(labels):
+            image_b, label_b = next(eval_generator)
+            labels[count] = label_b
+            count += 1
+        label_index = np.argmax(labels, axis=1)     
+        pred_index = np.argmax(predictions, axis=1)
+        np.set_printoptions(precision=2)
+        # Plot non-normalized confusion matrix
+        util.plot_confusion_matrix(label_index, pred_index, classes=np.array(list(util.indexed_dictionary)),
+                      class_dict=util.indexed_dictionary)
+        # Plot normalized confusion matrix
+        util.plot_confusion_matrix(label_index, pred_index, classes=np.array(list(util.indexed_dictionary)),
+                      class_dict=util.indexed_dictionary,
+                      normalize=True)
+        count = 0
+        for i in range(len(label_index)):
+            if(label_index[i] == pred_index[i]):
+                count+=1
+        print("Accuracy is {}".format(count/len(label_index)))
+        
